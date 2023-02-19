81ce11d4f526a2a2f54d255aaf10dea25bd1c6d7
ans1998 <ans1998@qq.com>
2023-02-19 21:11:56 +0800

    CR

---
+++ miniprogram/projects/A00/skin/skin.js
@@ -0,0 +1,43 @@
+module.exports = {
+	PID: 'A00', // 主题公园
+
+	NAV_COLOR: '#ffffff',
+	NAV_BG: '#FE5F5C',
+
+	MEET_NAME: '预约', 
+ 
+	MENU_ITEM: ['首页', '预约日历', '我的'], // 第1,4,5菜单
+
+	NEWS_CATE: '1=最新动态|leftbig,2=智慧导览|leftbig2,3=游玩攻略|leftbig,4=游览须知',
+	MEET_TYPE: '1=入园预约|leftbig2,2=停车预约|leftbig3',
+
+	DEFAULT_FORMS: [{
+			type: 'line',
+			title: '姓名',
+			desc: '请填写您的姓名',
+			must: true,
+			len: 50,
+			onlySet: {
+				mode: 'all',
+				cnt: -1
+			},
+			selectOptions: ['', ''],
+			mobileTruth: true,
+			checkBoxLimit: 2,
+		},
+		{
+			type: 'line',
+			title: '手机',
+			desc: '请填写您的手机号码',
+			must: true,
+			len: 50,
+			onlySet: {
+				mode: 'all',
+				cnt: -1
+			},
+			selectOptions: ['', ''],
+			mobileTruth: true,
+			checkBoxLimit: 2,
+		}
+	]
+}
